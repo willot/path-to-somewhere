8969868fa53c04232bddb03f86d673193b1acff5
Vianney <vianney.willot@gmail.com>
2024-01-20 11:18:48 -0500

    add routing on click

--- path-to-somewhere/app/page.test.tsx
+++ path-to-somewhere/app/page.test.tsx
@@ -2,30 +2,51 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Home from "./page";
 
+const mockPush = jest.fn();
+
+jest.mock('next/navigation', () => ({
+    ...jest.requireActual('next/navigation'),
+    useRouter: () => ({
+        push: mockPush,
+    })
+}));
+
 describe("home page", () => {
     it('should have an input to enter name', async () => {
-        render(<Home/>)
+        render(<Home />)
 
         const placeHolder = await screen.findByPlaceholderText("Enter a name");
-        
+
         expect(placeHolder).toBeInTheDocument();
     });
-    
+
     it('should have a disabled create character button when there is no name', async () => {
-        render(<Home/>)
-        
-        const button = await screen.findByRole('button', {name: 'Create your character'});
+        render(<Home />)
+
+        const button = await screen.findByRole('button', { name: 'Create your character' });
         expect(button).toBeDisabled();
     });
-    
+
     it('should have create character button enabled when user enter a name', async () => {
-        render(<Home/>)
-        
-        const placeHolder = await screen.findByPlaceholderText("Enter a name");     
+        render(<Home />)
+
+        const placeHolder = await screen.findByPlaceholderText("Enter a name");
         await userEvent.type(placeHolder, 'bob')
 
-        const button = await screen.findByRole('button', {name: 'Create your character'});
-        
+        const button = await screen.findByRole('button', { name: 'Create your character' });
+
         expect(button).not.toBeDisabled
+    })
+
+    it('should navigate to /profile when clicking on the create user button', async () => {
+        render(<Home />)
+
+        const placeHolder = await screen.findByPlaceholderText("Enter a name");
+        await userEvent.type(placeHolder, 'bob');
+
+        const button = await screen.findByRole('button', { name: 'Create your character' });
+        await userEvent.click(button);
+
+        expect(mockPush).toHaveBeenCalledWith('/profile');
     })
 })
--- path-to-somewhere/app/page.tsx
+++ path-to-somewhere/app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { useState } from "react";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
 
+  const router = useRouter();
   const [name, setName] = useState("");
 
   const isNameFormated = (): boolean => {
@@ -13,13 +15,19 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <input
-      className="text-black p-1 rounded-sm"
-        onChange={ (event) => {
+        className="text-black p-1 rounded-sm"
+        onChange={(event) => {
           setName(event.target.value)
         }}
-        placeholder="Enter a name" 
-        value={name}/>  
-        <button disabled={isNameFormated()}>Create your character</button>
+        placeholder="Enter a name"
+        value={name} />
+      <button
+        disabled={isNameFormated()}
+        onClick={() => {
+          router.push('/profile')
+        }}>
+        Create your character
+      </button>
     </main>
   );
 }
