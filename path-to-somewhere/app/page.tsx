aafcb0f357776056cc87c994cd0f31f36e187b80
Vianney <vianney.willot@gmail.com>
2024-01-20 11:36:05 -0500

    picking color is hard

--- path-to-somewhere/app/page.tsx
+++ path-to-somewhere/app/page.tsx
@@ -13,15 +13,17 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-8 p-24">
+      <h1 className="text-3xl font-bold text-cyan-600">Welcome to a new adventure</h1>
       <input
-        className="text-black p-1 rounded-sm"
+        className="text-black p-1 text-lg rounded-sm"
         onChange={(event) => {
           setName(event.target.value)
         }}
         placeholder="Enter a name"
         value={name} />
       <button
+      className="bg-cyan-600 p-3 text-lg rounded-full disabled:bg-gray-500 disabled:opacity-50 hover:bg-indigo-500"
         disabled={isNameFormated()}
         onClick={() => {
           router.push('/profile')
