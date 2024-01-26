92c71f7cefba0b76c7d0fd2f9f3bfb279775fb2b
Vianney <vianney.willot@gmail.com>
2024-01-26 13:05:13 -0500

    refactor UI

--- path-to-somewhere/app/profile/page.tsx
+++ path-to-somewhere/app/profile/page.tsx
@@ -8,6 +8,7 @@ import axios from 'axios';
 import {
     useQuery,
 } from '@tanstack/react-query'
+import ImageButton from "@/components/ImageButton";
 
 export type Character = 'wizard' | 'knight' | 'monk' | 'warrior' | undefined;
 
@@ -41,22 +42,10 @@ const Profile = () => {
             <h1 className="text-3xl font-bold text-cyan-600">Welcome {user?.userName}</h1>
             <h2 className="text-2xl font-bold">Select your character</h2>
             <div className="grid grid-cols-2 gap-3">
-                <button className="hover:opacity-50" aria-label="Wizard"
-                    onClick={() => { setSelection('wizard') }}>
-                    <img className="max-w-48 max-h-48 rounded-sm" src="/wizard.png" />
-                </button>
-                <button className="hover:opacity-50" aria-label="Knight"
-                    onClick={() => { setSelection('knight') }}>
-                    <img className="max-w-48 max-h-48 rounded-sm" src="/knight.png" />
-                </button>
-                <button className="hover:opacity-50" aria-label="Monk"
-                    onClick={() => { setSelection('monk') }}>
-                    <img className="max-w-48 max-h-48 rounded-sm" src="/monk.png" />
-                </button>
-                <button className="hover:opacity-50" aria-label="Warrior"
-                    onClick={() => { setSelection('warrior') }}>
-                    <img className="max-w-48 max-h-48 rounded-sm" src="/warrior.png" />
-                </button>
+                <ImageButton character={"wizard"} setter={setSelection} />
+                <ImageButton character={"knight"} setter={setSelection} />
+                <ImageButton character={"monk"} setter={setSelection} />
+                <ImageButton character={"warrior"} setter={setSelection} />
             </div>
             {selection && (
                 <>
---
+++ path-to-somewhere/components/ImageButton.tsx
@@ -0,0 +1,17 @@
+import { Character } from "@/app/profile/page"
+
+interface ImageButtonProps {
+    character: Character,
+    setter: (value: Character) => void,
+}
+
+const ImageButton = ({ character, setter }: ImageButtonProps) => {
+    return (
+        <button className="hover:opacity-50" aria-label={character}
+            onClick={() => { setter(character) }}>
+            <img className="max-w-48 max-h-48 rounded-sm" src={`/${character}.png`} />
+        </button>
+    )
+}
+
+export default ImageButton;
