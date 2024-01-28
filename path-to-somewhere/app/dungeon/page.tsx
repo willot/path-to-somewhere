86fb6d91ca40a7e9648d4b3543fe35329a937f80
Vianney <vianney.willot@gmail.com>
2024-01-28 15:54:16 -0500

    get rooms list from server to avoid order to change on state change

--- path-to-somewhere/app/dungeon/page.tsx
+++ path-to-somewhere/app/dungeon/page.tsx
@@ -4,6 +4,8 @@ import { useContext, useState } from "react";
 
 import dynamic from 'next/dynamic'
 import RoomButton from "@/components/RoomButton";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const Room1 = dynamic(() => import('@/components/Room1'), {
     loading: () => <p>Loading...</p>,
@@ -15,22 +17,31 @@ const Dungeon = () => {
     const user = useContext(UserContext)
     const [roomSelection, setRoomSelection] = useState<number | undefined>();
 
-    const rooms = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    const shuffle = (array: number[]) => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    };
+    const { isPending, error, data, isFetching } = useQuery({
+        queryKey: ['rooms'],
+        queryFn: () =>
+            axios
+                .get('http://localhost:3001/api/rooms')
+                .then((res) => res.data),
+    });
+
+    // const rooms = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    // const shuffle = (array: number[]) => {
+    //     for (let i = array.length - 1; i > 0; i--) {
+    //         const j = Math.floor(Math.random() * (i + 1));
+    //         [array[i], array[j]] = [array[j], array[i]];
+    //     }
+    //     return array;
+    // };
 
     return (
         <main className="flex min-h-screen flex-col items-center gap-8 p-24">
             <h1>Dungeon</h1>
             <h1 className="text-3xl font-bold text-cyan-600">Welcome {user?.userName} powerful {user?.character}</h1>
             <section className="grid grid-cols-3 gap-1">
-                {shuffle(rooms).map((room) => {
+                {data && (data.rooms.map((room: number) => {
                     return (
                         <RoomButton
                             key={room}
@@ -39,7 +50,7 @@ const Dungeon = () => {
                             setRoomSelection={setRoomSelection}
                         />
                     )
-                })}
+                }))}
             </section>
 
             {roomSelection && (
--- server/src/app.ts
+++ server/src/app.ts
@@ -17,6 +17,23 @@ app.get('/api/bio', (req, res) => {
     res.json ({
         characters: [wizardProfile, monkProfile, warriorProfile, knightProfile]
     })
+});
+
+app.get('/api/rooms', (req, res) => {
+
+    const rooms = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const shuffle = (array: number[]) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
+
+    res.json ({
+        rooms: shuffle(rooms)
+    })
 })
 
 // Start the server
