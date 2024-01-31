5bfd6f101b095c4fced1262a9166284abad1bd4f
Vianney <vianney.willot@gmail.com>
2024-01-30 21:12:54 -0500

    fix Roomceiling test

--- path-to-somewhere/components/RoomCeiling.test.tsx
+++ path-to-somewhere/components/RoomCeiling.test.tsx
@@ -2,6 +2,15 @@ import { render, screen } from "@testing-library/react"
 import RoomCeiling from "./RoomCeiling"
 import userEvent from "@testing-library/user-event"
 
+const mockPush = jest.fn();
+
+jest.mock('next/navigation', () => ({
+    ...jest.requireActual('next/navigation'),
+    useRouter: () => ({
+        push: mockPush,
+    })
+}));
+
 describe('Room1 component', () => {
 
     it('should display an image if you click enter the room', async() => {
@@ -14,15 +23,21 @@ describe('Room1 component', () => {
         expect(screen.getByRole('img', {name: "a big rock crushed you"})).toBeVisible;
     })
 
-    it('should trigger the setVital function when you enter the room and get crushed', async() => {
+    it('should see a restart button that get you to profile page when you enter the room and get crushed', async() => {
         const mockSetVitals = jest.fn();
-        render(<RoomCeiling vitals={{health:200, mana:50}} setVitals={mockSetVitals}/>)
+        render(<RoomCeiling vitals={{health:200, mana:50}} setVitals={mockSetVitals}/>);
 
-        const enterButton = screen.getByRole('button', {name: 'Enter'})
+        const enterButton = screen.getByRole('button', {name: 'Enter'});
 
         await userEvent.click(enterButton);
 
-        expect(mockSetVitals).toHaveBeenCalledWith({health:0, mana:50});
+        const deathText = screen.getByText('The ceiling fall on you and you die');
+        expect(deathText).toBeVisible();
+
+        const restartButton = screen.getByRole('button', {name: 'Restart'}) ;
+        await userEvent.click(restartButton);
+
+        expect(mockPush).toHaveBeenCalledWith("/profile");
     })
 
     it('should display some text if you click leave the room', async() => {
